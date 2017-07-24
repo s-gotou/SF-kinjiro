8992178afb6b5bb943e5df7c0bc3dc44ac487dad
DESKTOP-DFO1SAC\gotou <s_goto@style-free.co.jp>
2017-07-25 00:15:50 +0900

    新規追加

---
+++ Client/app/core/app.routes.ts
@@ -0,0 +1,28 @@
+﻿import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { WorkingRecordsComponent } from "../working-records/working-records.component";
+
+
+const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    // Lazy async modules
+    {
+        path: 'login', loadChildren: './+login/login.module#LoginModule'
+    },
+    {
+        path: 'register', loadChildren: './+register/register.module#RegisterModule'
+    },
+    {
+        path: 'profile', loadChildren: './+profile/profile.module#ProfileModule'
+    },
+    {
+        path: 'admin', loadChildren: './+admin/admin.module#AdminModule'
+    },
+    {
+        path: 'examples', loadChildren: './+examples/examples.module#ExamplesModule'
+    },
+    {
+        path: 'working-records', component: WorkingRecordsComponent
+    }
+];
+
+export const routing = RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules });
